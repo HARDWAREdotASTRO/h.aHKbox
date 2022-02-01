d26943d38c96a9463e640cd51e8beb73e5ccf2de
Stephen Shaw <stephen.shaw120@gmail.com>
2022-02-01 15:46:13 -0600

    Update RMSreadInNAU_firmWareV1_17Nov2021.ino
    Added comments, revised code to allow for different frequency inputs besides 1 Hz.

--- 4Wire_AC/LabFiles/LAB005_DigitalAdditions1/RMSreadInNAU_firmWareV1_17Nov2021/RMSreadInNAU_firmWareV1_17Nov2021.ino
+++ 4Wire_AC/LabFiles/LAB005_DigitalAdditions1/RMSreadInNAU_firmWareV1_17Nov2021/RMSreadInNAU_firmWareV1_17Nov2021.ino
@@ -10,25 +10,25 @@
 
 Adafruit_NAU7802 nau;
 
-float twopi = 3.14159 * 2;
-float freq_m = 1; // Frequency of Modulation (Hz), [1, 160] range allowed given Nyquist sampling rate of 2 samples per period, 320 SPS
-float rms_sum, tim;
-int i;
-int samps = (320 / freq_m); //Number of samples per frame of period
-float voltRead [319]; // Array of voltage values read
+float twopi = 3.14159 * 2; //For sin wave generation
+float freq_m = 1; // Frequency of Modulation (Hz), [1, 160] range allowed given Nyquist sampling rate of 2 samples per period, 320 max SPS
+float rms_sum, tim; //rms sum for RMS calculations, tim for outputting sinwave based on time
+int i; //integer used to keep track of data set per read cycle (1 sine wave cycle)
+int samps = int (320 / freq_m); //Number of samples per read cycle ((320 samples/sec) / (1/sec)) = samples
+float voltRead [320]; // Array of voltage values read per read cycle (set to max readings if frequency is 1 Hz)
 
 void setup() {
   // put your setup code here, to run once:
-  analogWriteResolution(12);
+  analogWriteResolution(12); //Define DAC output pin for sin wave generation
 
-  Serial.begin(115200);
-  while ( !Serial ) ;
-  Serial.println("NAU7802");
+  Serial.begin(115200); //Begin serial comm for debug
+  while ( !Serial ) ; //Wait for serial comm to initialize
+  // Error handling if ADC not found. Might need to run nauStartupCode or check wiring
   if (! nau.begin()) {
     Serial.println("Failed to find NAU7802");
   }
-  // Serial.println("Found NAU7802");
 
+  // Define ADC settings
   nau.setLDO(NAU7802_EXTERNAL);
   nau.setGain(NAU7802_GAIN_1);
   Serial.print("Gain set to ");
@@ -44,12 +44,13 @@ void setup() {
   }
   nau.setRate(NAU7802_RATE_320SPS);
 
-  // Take 10 readings to flush out readings
-  for (uint8_t i = 0; i < 10; i++) {
+  // Take 100 readings to flush out ADC
+  for (uint8_t i = 0; i < 100; i++) {
     while (! nau.available()) delay(1);
     nau.read();
   }
 
+  // Do system calibrations
   while (! nau.calibrate(NAU7802_CALMOD_INTERNAL)) {
     Serial.println("Failed to calibrate internal offset, retrying!");
     delay(1000);
@@ -63,33 +64,39 @@ void setup() {
   Serial.println("Calibrated system offset");
   nau.setChannel(NAU7802_CH1);
 
+  //Define variables at starting values for RMS and read cycle looping
   rms_sum = 0;
   i = 0;
 }
 
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  tim = millis() / 1000.0; //Calculate Microseconds
-  Sin(freq_m, tim); //Continue Sin wave output
+  tim = millis() / 1000.0; //Calculate Seconds
+  Sin(freq_m, tim); // Run next sinwave output voltage level based on frequency and time
 
-  if (nau.available() == 1) //If 1/320 seconds have passed, enter loop (new reading available)
+  if (nau.available() == 1) //If new data available(1/320 seconds have passed), enter loop
   {
-    voltRead[i % (samps)] = ((nau.read() / 8388608.0) * 2.5); //Read ADC
-    if (i == 320) //If enought readings (full period, 320 samples), then calculate and send RMS. Reset variables
+    voltRead[i] = ((nau.read() / 8388608.0) * 2.5); //Read ADC, equation set by hardware. See data sheet for NAU7802
+    i = i + 1;
+    if (i == samps) //If enought readings (full period, 320 samples), then calculate and send RMS. Reset variables
     {
       for (int k = 0; k < samps; k++) { //Calcuate RMS sum pre-sqrt
         rms_sum = voltRead[k] * voltRead[k] + rms_sum;
+        //Serial.println(k);
+        //Serial.println("");
+        //Serial.println(voltRead[k], 4); Serial.print(",");
+        //Serial.println("");
       }
-      i = 0;
-      float rms_volt = sqrt(rms_sum/320.0);
+      float rms_volt = sqrt(rms_sum / float(samps));
       Serial.println("RMS_Voltage");
       Serial.println("");
       Serial.println(rms_volt, 6);
       Serial.println("");
+      Serial.print(tim, 7);
+      Serial.println("");
       rms_sum = 0;
+      i = 0;
     }
-    i = i + 1;
   }
 }
 
