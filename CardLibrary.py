dba5537a4c4b8552346383bb930ddabd6dadb72a
Larry <80938629+Larry-vt@users.noreply.github.com>
2022-07-01 12:34:13 -0500

    created Card Library python module
    we may not need an entirely separate module for handing card information and formatting but currently it might be a good idea as to keep things a bit more organized and less confusing and cluttered. that way the h.ahkBox.py can focus on its baseline function and communication. All while this module can be used to interpret the byte data from given cards and translate the bytes into a format that can be displayed on the GUI and placed into a file to be downloaded and viewed. 
    
    note: (may or may not be needed) we may also need to have a seprate file that has the raw byte information saved as well so that incase there is weird error we can see bit by bit where that error could be occuring-

---
+++ CardLibrary.py
@@ -0,0 +1,19 @@
+'''
+    The purpose of this module is to act as data organization for various card types
+    this is used by the GUI to interpret the data being set by the h.ahk Box API bytes
+    and organizated the information as appropriate for each card, this allows us to break down up the bytes of data
+    into the correct interpreted values
+
+    it will also tell the GUI what cards everything is and tell it interface it should display in 
+    in the tab for each card
+
+'''
+
+# function:
+#   card interface boot()
+
+# function
+#   interpret information
+
+# function
+#   error organization
