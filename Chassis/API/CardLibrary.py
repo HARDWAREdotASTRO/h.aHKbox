cd54f3e7b722e5b080abbd76fc31aaa2c764e1d7
Larry <80938629+Larry-vt@users.noreply.github.com>
2022-07-19 12:49:29 -0500

    Pseudo code for card library
    currently still trying to figure how I want data to be send
    right now i have is so there will be a separate function that will interpret what card it is and the from there it will go to a specific card function and based on the logistical header it will then perform its specific logistical header operation

---
+++ Chassis/API/CardLibrary.py
@@ -0,0 +1,34 @@
+'''
+    The purpose of this module is to act as data organization for various card types
+    this is used by the GUI to interpret the data being set by the h.ahk Box API bytes
+    and organizated the information as appropriate for each card, this allows us to break down up the bytes of data
+    into the correct interpreted values
+
+    it will also tell the GUI what cards everything is and tell it interface it should display in 
+    in the tab for each card
+
+'''
+
+'''
+    # sort out what the card is and use the corresponding 
+    card sort
+'''
+
+# function:
+# cards will be their own function and perform certain actions based on the logistical header
+def wireAC():
+    '''
+    if bit header is boot do tihs:
+
+    if bit header is want information do this:
+
+    if bit header is shutdown do this:
+    
+    if bit header is error do this:
+    
+
+    '''
+    ...
+
+
+
