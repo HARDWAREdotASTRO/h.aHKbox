6fd6f9412849fab54eeb34f4b468b05c0fab949b
Larry <80938629+Larry-vt@users.noreply.github.com>
2022-07-19 18:48:45 -0500

    Pseudo code idea one

--- Chassis/API/CardLibrary.py
+++ Chassis/API/CardLibrary.py
@@ -13,22 +13,102 @@
     # sort out what the card is and use the corresponding 
     card sort
 '''
+#create a byte array
+
 
 # function:
 # cards will be their own function and perform certain actions based on the logistical header
-def wireAC():
-    '''
-    if bit header is boot do tihs:
+# bits holds the byte array
+def wireAC(bits):
+    ch1 = 0
+    ch2 = 0
+    ch3 = 0
+    ch4 = 0
+    queByteData = bytearray([])
 
-    if bit header is want information do this:
 
-    if bit header is shutdown do this:
+
+
+    # signal to talk
+    if((bits[0] & 0xf0) == 0b00000 << 4):
+        
+
+        ...
+
     
-    if bit header is error do this:
+    #byte request
+    if((bits[0] & 0xf0) == 0b00001 << 4):
+
+
+        ...
     
 
-    '''
-    ...
+    # error
+    if((bits[0] & 0xf0) == 0b00010 << 4):
+
+
+        ...
+
+    
+    # data
+    if((bits[0] & 0xf0) == 0b00011 << 4):
+        #0b1000 -- ch1
+        #0b0100 -- ch2
+        #0b0010 -- ch3
+        #0b0001 -- ch4
+        #0b0000 -- all channels are off
+        if((bits[1] & 0x80) == 0b1000 << 4):
+            print("ch1 is on")
+            if((bits[1] & 0x08) == 0b1000 ):
+                print("voltage")
+            else:
+                print("Resistance")
+
+
+        if((bits[1] & 0x40) == 0b0100 << 4):
+            print("ch2 is on")
+            if((bits[1] & 0x04) == 0b0100 ):
+                print("voltage")
+            else:
+                print("Resistance")
+            print("range: " + bits[2])
+            
 
 
 
+        if((bits[1] & 0x20) == 0b0010 << 4):
+            print("ch3 is on")
+            if((bits[1] & 0x02) == 0b0010 ):
+                print("voltage")
+            else:
+                print("Resistance")
+            
+    
+        if((bits[1] & 0x10) == 0b0001 << 4):
+            print("ch4 is on")
+            if((bits[1] & 0x01) == 0b0001 ):
+                print("voltage")
+            else:
+                print("Resistance")
+
+        return ch1, ch2, ch3, ch4
+
+
+
+
+
+
+
+        
+    #command response
+    if((bits[0] & 0xf0) == 0b00111 << 4):
+        # this portion of code will be responsible for handling user input responses. 
+        # this will allow the user to turn a channel off, export channel data, and to switch between voltages and resistance values
+
+
+
+
+        ...
+
+test = bytearray([0x33, 0xff, 0x02, 0x78, 0xdf, 0x3a, 0x55])
+wireAC(test)
