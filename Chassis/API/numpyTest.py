c700d285422a82c32ebd6cd671020d11a98bb441
Larry <80938629+Larry-vt@users.noreply.github.com>
2022-08-02 17:06:06 -0500

    numpy works with Card library
    got the test file to write and create a text file as we want it. however, the data is very layered from all the parsing and organizing it needs to be cleaned up carefully. (example is line 21)

--- Chassis/API/numpyTest.py
+++ Chassis/API/numpyTest.py
@@ -6,29 +6,31 @@ import datetime
 date = datetime.datetime.now()
 voltRes = 'Resistance'
 timearray = np.array(['Time'])
+cardarray = np.array(['Card Type'])
+temparray = np.array(['Temperature'])
 xarray = np.array(['channel 1'])
 yarray = np.array(['channel 2'])
-ch3array = np.array(['channel 3'])
+ch1array = np.array(['channel 1'])
 ch4array = np.array(['channel 4'])
 i = 0
 
 test = bytearray([0x33, 0xff,    0x01, 0x02, 0xa3, 0x04, 0x05,    0x06, 0x07, 0x0b8, 0x09, 0x0a,     0x0b, 0x0c, 0xcd, 0x0e, 0x0f,      0x10, 0x11, 0xd2, 0x13, 0x14])
-while i < 5:
+t = wireAC(test)
+timearray = np.append(timearray, str(date))
+cardarray = np.append(cardarray, (test[0] & 0x0f))
+temparray = np.append(temparray, str(t[0][0][3]))
+ch1array = np.append(ch1array, str(t[0]))
 
-    timearray = np.append(timearray, date)
-    xarray = np.append(xarray, wireAC(test)[0])
-    yarray = np.append(yarray, wireAC(test)[1])
-    ch3array = np.append(ch3array, wireAC(test)[2])
-    ch4array = np.append(ch4array, wireAC(test)[3])
-    i = i + 1
+i = i + 1
 
 
 # here is your data, in two numpy arrays
 
-data = np.column_stack([xarray, yarray, ch3array, ch4array,])
+data = np.column_stack([timearray, cardarray, temparray, ch1array])
+print(type(t[1][3]))
 datafile_path = "/Users/hy2755lw/Dropbox/4WireAC.txt "
 
-np.savetxt(datafile_path , data, fmt=['%d','%d','%d','%d'])
+np.savetxt(datafile_path , data, fmt=['%s','%s','%s','%s'])
 # here the ascii file is written. 
 
 #file = np.savetxt("4AC-ch1.txt", ch1, fmt='%s')
