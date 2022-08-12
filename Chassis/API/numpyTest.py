e991e5ab1dd2c525d2f8fad04183542dcfbf5f10
Larry <80938629+Larry-vt@users.noreply.github.com>
2022-08-12 13:27:04 -0500

    updated for current version of card Library

--- Chassis/API/numpyTest.py
+++ Chassis/API/numpyTest.py
@@ -1,5 +1,5 @@
 import numpy as np
-from CardLibrary import wireAC
+from CardLibrary import cardType
 import datetime
 
 
@@ -12,30 +12,33 @@ temparray = np.array(['Temperature'])
 xarray = np.array(['channel 1'])
 yarray = np.array(['channel 2'])
 ch1array = np.array(['channel 1'])
-ch4array = np.array(['channel 4'])
+#ch1array = np.array(['channel 2'])
+tv = 1
 i = 0
+while i < 10:
+    test = bytearray([0x33, 0xff,    0x01, tv, 0xa3, 0x04, 0x05,    0x06, 0x07, 0x0b8, 0x09, 0x0a,     0x0b, 0x0c, 0xcd, 0x0e, 0x0f,      0x10, 0x11, 0xd2, 0x13, 0x14])
+    #test2 = bytearray([0x33, 0xff,    0x01, tv, 0xa3, 0x04, 0x05,    0x06, 0x07, 0x0b8, 0x09, 0x0a,     0x0b, 0x0c, 0xcd, 0x0e, 0x0f,      0x10, 0x11, 0xd2, 0x13, 0x14])
+    t = cardType(test)
+    timearray = np.append(timearray, str(date))
+    cardarray = np.append(cardarray, (test[0] & 0x0f))
+    temparray = np.append(temparray, str(t[0][0][0]))
+    ch1array = np.append(ch1array, str(t[0][0])+ '\n')
+    tv = 4
 
-test = bytearray([0x33, 0xff,    0x01, 0x02, 0xa3, 0x04, 0x05,    0x06, 0x07, 0x0b8, 0x09, 0x0a,     0x0b, 0x0c, 0xcd, 0x0e, 0x0f,      0x10, 0x11, 0xd2, 0x13, 0x14])
-t = wireAC(test)
-timearray = np.append(timearray, str(date))
-cardarray = np.append(cardarray, (test[0] & 0x0f))
-temparray = np.append(temparray, str(t[0][0][3]))
-ch1array = np.append(ch1array, str(t[0][0]))
+    #i = i + 1
 
-i = i + 1
+    # here is your data, in two numpy arrays
 
-
-# here is your data, in two numpy arrays
-
-data = np.column_stack([timearray, cardarray, temparray, ch1array]) 
-print(type(t[1][3]))
-datafile_path = "/Users/hy2755lw/Dropbox/" + TodaysDate+"-4WireAC.txt"
-#a = open(datafile_path, 'a')
-np.savetxt(datafile_path , data , fmt=['%s','%s','%s','%s'])
+    data = np.column_stack([timearray, cardarray, temparray, ch1array]) 
+    print(type(t[1][0]))
+    datafile_path = "/Users/hy2755lw/Dropbox/" + TodaysDate+"-4WireAC.txt"
+    #a = open(datafile_path, 'a')
+    a = open(datafile_path, 'a+')
+    np.savetxt(datafile_path , data , fmt=['%s','%s','%s','%s'])
+    i = i+1
 # here the ascii file is written. 
 
 #file = np.savetxt("4AC-ch1.txt", ch1, fmt='%s')
-a = open(datafile_path, 'a')
-a = open(datafile_path, 'r')
+#a = open(datafile_path, 'r')
 print(a.read())
 a.close()
