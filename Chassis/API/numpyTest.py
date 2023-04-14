c242a30ec1bbaeb115f8e6460fbf237f73f98270
Laramie (Larry) Vettleson-Trutza <80938629+Larry-vt@users.noreply.github.com>
2023-04-14 09:52:26 -0500

    Working Test file!
    Figured out the issue
    because of previous debugging I forgot to add in previous arrays (ch1array, ch2array, etc) into the data variable so it would not match the number of %s in the fmt portion of savetxt. 
    The program now creates a new a file every day 
    for future updates it should be figured out how to handle this much data and how much and how often should the data should be stored so that there isn't too much data in the file each day or figure out a smaller file type instead of txt

--- Chassis/API/numpyTest.py
+++ Chassis/API/numpyTest.py
@@ -1,5 +1,6 @@
 import numpy as np
 from CardLibrary import cardType
+#reload(cardType)
 import datetime
 
 
@@ -7,34 +8,73 @@ date = datetime.datetime.now()
 TodaysDate = str(datetime.datetime.now().month) + str(datetime.datetime.now().day) + str(datetime.datetime.now().year)
 voltRes = 'Resistance'
 timearray = np.array(['Time'])
-cardarray = np.array(['Card Type'])
+cardarray = np.array(['Card_Type'])
 temparray = np.array(['Temperature'])
 xarray = np.array(['channel 1'])
 yarray = np.array(['channel 2'])
 ch1array = np.array(['channel 1'])
+ch2array = np.array(['channel 2'])
+ch3array = np.array(['channel 3'])
+ch4array = np.array(['channel 4'])
+print(temparray)
 #ch1array = np.array(['channel 2'])
 tv = 1
 i = 0
 while i < 10:
     test = bytearray([0x33, 0xff,    0x01, tv, 0xa3, 0x04, 0x05,    0x06, 0x07, 0x0b8, 0x09, 0x0a,     0x0b, 0x0c, 0xcd, 0x0e, 0x0f,      0x10, 0x11, 0xd2, 0x13, 0x14])
-    #test2 = bytearray([0x33, 0xff,    0x01, tv, 0xa3, 0x04, 0x05,    0x06, 0x07, 0x0b8, 0x09, 0x0a,     0x0b, 0x0c, 0xcd, 0x0e, 0x0f,      0x10, 0x11, 0xd2, 0x13, 0x14])
-    t = cardType(test)
+    test = bytearray([0x33, 0x88,    0x01, 0x02, 0xa3, 0x04, 0x05])
+    '''
+    0011 0011  1111 1111
+    0000 0001  0000 0001  1010 0011  0000 0100  0000 0101
+
+    '''
+    test = bytearray([0x33, 0xff,    0x01, 0x02, 0xa3, 0x04, 0x05,    0x06, 0x07, 0x0b8, 0x09, 0x0a,     0x0b, 0x0c, 0xcd, 0x0e, 0x0f,      0x10, 0x11, 0xd2, 0x13, 0x14])
+
+    test2 = bytearray([0x33, 0x88,    0x01, 0x02, 0xa3, 0x04, 0x05])
+    #test2 = bytearray([0x33, 0x88,    0x01, tv, 0xa3, 0x04, 0x05])
+    t = cardType(test2)
+
+    #adds date to arrsy
     timearray = np.append(timearray, str(date))
-    cardarray = np.append(cardarray, (test[0] & 0x0f))
-    temparray = np.append(temparray, str(t[0][0][0]))
+    cardarray = np.append(cardarray, str(test[0] & 0x0f))
+    # temperature values
+    print("testing to see the value \n")
+    print(t[0][0])
+    print(type(t[0]))
+    temparray = np.append(temparray, str(t[0])+ '\n')
     ch1array = np.append(ch1array, str(t[0][0])+ '\n')
-    tv = 4
+    ch2array = np.append(ch2array, str(t[0][0])+ '\n')
+    
+    ch3array = np.append(ch3array, str(t[0][0])+ '\n')
+    ch4array = np.append(ch4array, str(t[0][0])+ '\n')
+
+    # timearray = np.array(date)
+    # cardarray = np.array(test[0] & 0x0f)
+    # temparray = np.array( t)
+    # ch1array = np.array( ch1array, t[0][0]+ '\n')
+    # ch2array = np.array(ch2array, str(t[0][0])+ '\n')
+    
+    # ch3array = np.array(ch3array, str(t[0][0])+ '\n')
+    # ch4array = np.array(ch4array, str(t[0][0])+ '\n')
+    tv = 1
 
     #i = i + 1
 
     # here is your data, in two numpy arrays
 
-    data = np.column_stack([timearray, cardarray, temparray, ch1array]) 
-    print(type(t[1][0]))
-    datafile_path = "/Users/hy2755lw/Dropbox/" + TodaysDate+"-4WireAC.txt"
+
+    #remeber to add back the data
+    data = np.column_stack([timearray, cardarray, temparray, ch1array, ch2array, ch3array, ch4array]) 
+    #print(type(t[1][3]))
+    '''
+    an important thing to not here is this is a temporary file location 
+    and will need to be changed to fit your computer destination!
+
+    '''
+    datafile_path = "/Users/hy2755lw/Dropbox/" + TodaysDate + "-4WireAC.txt"
     #a = open(datafile_path, 'a')
     a = open(datafile_path, 'a+')
-    np.savetxt(datafile_path , data , fmt=['%s','%s','%s','%s'])
+    np.savetxt(datafile_path , data , delimiter= ',', fmt=['%s','%s','%s','%s','%s','%s','%s'])
     i = i+1
 # here the ascii file is written. 
 
